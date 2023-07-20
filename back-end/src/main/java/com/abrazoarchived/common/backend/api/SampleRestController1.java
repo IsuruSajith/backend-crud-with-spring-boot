070f07d4089c4677a9e5fc5f306c149751d9ceff
Isuru <isurusajith0@gmail.com>
2023-07-20 13:15:57 +0530

    creat a common controller class for api layer to get all front end request and check the services using postman

---
+++ back-end/src/main/java/com/abrazoarchived/common/backend/api/SampleRestController1.java
@@ -0,0 +1,28 @@
+package com.abrazoarchived.common.backend.api;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/samples")
+public class SampleRestController1 {
+    @GetMapping
+    public void getSample() {
+        System.out.println("get");
+    }
+
+    @DeleteMapping
+    public void deleteSample() {
+        System.out.println("delete");
+    }
+
+    @PatchMapping
+    public void updateSample() {
+        System.out.println("patch");
+    }
+
+    @PostMapping
+    public void saveSample() {
+        System.out.println("post");
+    }
+
+}
