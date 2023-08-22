0595a4e7c8b081b5000a64b01046ee326b039dfb
Isuru <isurusajith0@gmail.com>
2023-08-22 15:35:24 +0530

    implemet repository layer and save the data came from the front end..
    but still do not use model mapper to map dto to entity, just make the object from entity and insert via getters and setter methods in the dto class

--- back-end/src/main/java/com/abrazoarchived/common/backend/service/impl/StudentServiceImpl.java
+++ back-end/src/main/java/com/abrazoarchived/common/backend/service/impl/StudentServiceImpl.java
@@ -1,19 +1,25 @@
 package com.abrazoarchived.common.backend.service.impl;
 
 import com.abrazoarchived.common.backend.dto.StudentDTO;
+import com.abrazoarchived.common.backend.entity.Student;
 import com.abrazoarchived.common.backend.repository.StudentRepository;
 import com.abrazoarchived.common.backend.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.validation.constraints.NotBlank;
+import java.sql.Date;
+import java.util.ArrayList;
+
 @Service
 public class StudentServiceImpl implements StudentService {
     @Autowired
     private StudentRepository studentRepository;
+
     @Override
     public String saveStudent(StudentDTO studentDTO) {
-
-
+        Student student = new Student(studentDTO.getNic(), studentDTO.getFullName(), studentDTO.getAddress(), studentDTO.getAdmissionNo(), studentDTO.getAddmissionDate(), studentDTO.getCourses(), studentDTO.getFaculty(), studentDTO.getContactNumbers(), studentDTO.isActiveStatus());
+        studentRepository.save(student);
 
         return studentDTO.getFullName();
     }
