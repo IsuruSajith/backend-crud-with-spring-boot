ed933c93aa2c58967758019431a9467b8f459671
Isuru <isurusajith0@gmail.com>
2023-08-22 15:27:25 +0530

    injected repository to service impl class

--- back-end/src/main/java/com/abrazoarchived/common/backend/service/impl/StudentServiceImpl.java
+++ back-end/src/main/java/com/abrazoarchived/common/backend/service/impl/StudentServiceImpl.java
@@ -1,14 +1,20 @@
 package com.abrazoarchived.common.backend.service.impl;
 
 import com.abrazoarchived.common.backend.dto.StudentDTO;
+import com.abrazoarchived.common.backend.repository.StudentRepository;
 import com.abrazoarchived.common.backend.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class StudentServiceImpl implements StudentService {
+    @Autowired
+    private StudentRepository studentRepository;
     @Override
     public String saveStudent(StudentDTO studentDTO) {
-        System.out.println(studentDTO.toString());
+
+
+
         return studentDTO.getFullName();
     }
 }
