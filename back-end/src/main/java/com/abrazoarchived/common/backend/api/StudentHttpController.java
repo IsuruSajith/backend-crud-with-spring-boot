3be278421808b82a579a1e95263b2804a4821bf5
Isuru <isurusajith0@gmail.com>
2023-08-22 12:50:14 +0530

    experiencing restcontroller vs controller and requestbody annotaions

--- back-end/src/main/java/com/abrazoarchived/common/backend/api/StudentHttpController.java
+++ back-end/src/main/java/com/abrazoarchived/common/backend/api/StudentHttpController.java
@@ -1,6 +1,8 @@
 package com.abrazoarchived.common.backend.api;
 
+import com.abrazoarchived.common.backend.dto.StudentDTO;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,7 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class StudentHttpController {
 
     @PostMapping
-    public String saveStudent() {
-        return "saved";
+    public String saveStudent(@RequestBody StudentDTO studentDTO) {
+        System.out.println(studentDTO.getFullName());
+        return studentDTO.getFullName();
     }
 }
