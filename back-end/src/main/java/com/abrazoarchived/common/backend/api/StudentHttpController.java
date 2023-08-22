5f0319c35d9a0ac98e5156c669c49a51e792ee5b
Isuru <isurusajith0@gmail.com>
2023-08-22 14:46:27 +0530

    impl class made as a service class.
    and controller class access the interface form
    
     @Autowired
        private StudentService studentService;

--- back-end/src/main/java/com/abrazoarchived/common/backend/api/StudentHttpController.java
+++ back-end/src/main/java/com/abrazoarchived/common/backend/api/StudentHttpController.java
@@ -1,7 +1,10 @@
 package com.abrazoarchived.common.backend.api;
 
 import com.abrazoarchived.common.backend.dto.StudentDTO;
+import com.abrazoarchived.common.backend.service.SampleService;
+import com.abrazoarchived.common.backend.service.StudentService;
 import com.abrazoarchived.common.backend.service.impl.StudentServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,11 +13,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("api/v1/student")
 public class StudentHttpController {
+    @Autowired
+    private StudentService studentService;
     @PostMapping
     public StudentDTO saveStudent(@RequestBody StudentDTO studentDTO) {
-        StudentServiceImpl studentService = new StudentServiceImpl();
+        //StudentServiceImpl studentService = new StudentServiceImpl();
+        //studentService.saveStudent(studentDTO);
+        //System.out.println(studentService);
+
         studentService.saveStudent(studentDTO);
-        System.out.println(studentService);
+
         return studentDTO;
     }
 }
--- back-end/src/main/java/com/abrazoarchived/common/backend/service/impl/StudentServiceImpl.java
+++ back-end/src/main/java/com/abrazoarchived/common/backend/service/impl/StudentServiceImpl.java
@@ -2,7 +2,9 @@ package com.abrazoarchived.common.backend.service.impl;
 
 import com.abrazoarchived.common.backend.dto.StudentDTO;
 import com.abrazoarchived.common.backend.service.StudentService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class StudentServiceImpl implements StudentService {
     @Override
     public void saveStudent(StudentDTO studentDTO) {
