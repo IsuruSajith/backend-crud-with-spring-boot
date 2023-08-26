77a75692a9cd6d8dd6e55122e5693499a8fec1b6
Isuru <isurusajith0@gmail.com>
2023-08-26 10:07:18 +0530

    going to convert generics

--- back-end/src/main/java/com/abrazoarchived/common/backend/service/SampleService.java
+++ back-end/src/main/java/com/abrazoarchived/common/backend/service/SampleService.java
@@ -5,10 +5,14 @@ import com.abrazoarchived.common.backend.dto.SampleDTO;
 import java.util.List;
 
 public interface SampleService {
-
     void saveSample(SampleDTO sample);
     void updateSample(SampleDTO sample);
     void deleteSample(String regNo);
     SampleDTO getSample(String regNo);
     List<SampleDTO> findSample(String query);
+
+
+
+
+
 }
--- back-end/src/main/java/com/abrazoarchived/common/backend/service/UniversityService.java
+++ back-end/src/main/java/com/abrazoarchived/common/backend/service/UniversityService.java
@@ -4,4 +4,19 @@ import com.abrazoarchived.common.backend.dto.UniversityDTO;
 
 public interface UniversityService {
     String save(UniversityDTO universityDTO);
+
+
+    /*long count() throws Exception;
+
+    T save(T entity) throws Exception;
+
+    void update(T entity) throws Exception;
+
+    void deleteById(ID pk)throws Exception;
+
+    Optional<T> findById(ID pk) throws Exception;
+
+    List<T> findAll() throws Exception;
+
+    boolean existsById(ID pk) throws Exception;*/
 }
