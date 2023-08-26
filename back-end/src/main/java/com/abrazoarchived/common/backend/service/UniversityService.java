7149be9a0c8d0f30cdae22387faf33a03613655f
Isuru <isurusajith0@gmail.com>
2023-08-26 10:17:11 +0530

    created facade layer for loosly couple service layer from crud methods

--- back-end/src/main/java/com/abrazoarchived/common/backend/service/UniversityService.java
+++ back-end/src/main/java/com/abrazoarchived/common/backend/service/UniversityService.java
@@ -6,17 +6,5 @@ public interface UniversityService {
     String save(UniversityDTO universityDTO);
 
 
-    /*long count() throws Exception;
 
-    T save(T entity) throws Exception;
-
-    void update(T entity) throws Exception;
-
-    void deleteById(ID pk)throws Exception;
-
-    Optional<T> findById(ID pk) throws Exception;
-
-    List<T> findAll() throws Exception;
-
-    boolean existsById(ID pk) throws Exception;*/
 }
